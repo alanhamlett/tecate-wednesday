4ca37522a4c21d9b069e4b2ce90d7f6bf790b06e
Alan Hamlett <alan.hamlett@prezi.com>
2013-10-02 22:03:16 -0700

    added history file

--- 001.py
+++ 001.py
@@ -4,4 +4,8 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000."""
 
-
+summ = 0
+for n in range(0, 1000):
+    if n % 3 == 0 or n % 5 == 0:
+        summ += n
+print summ
---
+++ HISTORY.rst
@@ -0,0 +1,10 @@
+
+History
+-------
+
+
+2013-10-02
+++++++++++
+
+- 1 point, completed up to problem 1
+
