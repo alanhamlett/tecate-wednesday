0ed0fc1cccf3dd7feece5793735715cc0d5c57a1
Alan Hamlett <alan.hamlett@prezi.com>
2013-10-02 22:47:09 -0700

    finished 048

--- 048.py
+++ 048.py
@@ -1,6 +1,12 @@
 """Problem 48
-The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
-Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000."""
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000."""
 
+highest = 1000
 
+total = 0
+for n in range(1, highest):
+    total += pow(n, n)
+    print total
+print total
